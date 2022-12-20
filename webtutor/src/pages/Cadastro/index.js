7d72d8b89aaed0476aa77e6c0f93d3cf6843ed5f
deyvisongdias <deyvisondias@hotmail.com>
2022-12-20 00:31:11 -0300

    Finalizando cadastro

--- webtutor/src/pages/Cadastro/index.js
+++ webtutor/src/pages/Cadastro/index.js
@@ -3,12 +3,20 @@ import Logo from "./logo.PNG";
 import './index.css';
 
 function Cadastro() {
+
+  var optionAluno=document.getElementById("aluno");
+  var optionProfessor=document.getElementById("professor");
+  var typeOfData=document.getElementById("documentOf");
+
+ 
+  
+
   return (
+
     <div>
       <div class="box">
-        <div class="box-form">
-        
-        <form action="">
+        <div class="box-form">     
+        <form action="" method="">
         <h2>Cadastro de Usúario</h2>
         <input type="text" id="nome" placeholder="Nome"></input>
         <input  type="text" id="email" placeholder="E-mail"></input>
@@ -16,16 +24,22 @@ function Cadastro() {
         <input type="text" id="wpp" placeholder="Whatsapp"></input>
         <input  type="text" id="formacao" placeholder="Formação"></input>
         <h5>Voce e?</h5>
-        <input type="radio" id="aluno" name="Aluno" value="Aluno" />
-        <label for="aluno">Aluno</label>
-        <input type="radio" id="Professor" name="Professor" value="Professor" />
-        <label for="professor">Professor</label><br></br>
-        <button type="submit" >Registrar-se</button><br></br>
-        </form>
-
+        <div>
+        <select name="Tipo de usuario" required>
+        <option value="Aluno" id="aluno">Aluno</option>
+        <option value="professor" id="professor" >Professor</option>
+        </select>    
+        
         </div>
-      </div>
-    </div>
+        <input type="text" id="documentOf"/>
+        
+        <button type="submit">Registrar-se</button><br></br>
+        
+        
+        </form>
+        </div>
+        </div>
+        </div>
   );
 }
 
--- webtutor/src/pages/Login/index.js
+++ webtutor/src/pages/Login/index.js
@@ -5,11 +5,11 @@ import "./ComponentButton"
 import Logo from "./logo.PNG";
 
 function Login() {
+
   return (
     <div>
         <div class="box">
             
-           
             <div class="box-form">
             <img src={Logo} class="logo"></img>
             <form >
@@ -26,6 +26,7 @@ function Login() {
 
             <button>Login</button>
             <button>Cadastrar-se</button>
+      
         </form>
 
             </div>
