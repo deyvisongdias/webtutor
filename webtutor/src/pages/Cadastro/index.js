b765030651dd01331a74816846ba2006608a173e
deyvisongdias <deyvisondias@hotmail.com>
2022-12-20 22:32:39 -0300

    Formatando arquivos

--- webtutor/src/pages/Cadastro/index.js
+++ webtutor/src/pages/Cadastro/index.js
@@ -3,35 +3,31 @@ import './index.css';
 
 function Cadastro() {
 
-
-
   return (
-
     <div>
       <div class="box">
-        <div class="box-form">     
-        <form action="" method="POST" id="registerForm">
-        <h2>Cadastro de Usúario</h2>
-        <input type="text" id="nome" placeholder="Nome"></input>
-        <input  type="text" id="email" placeholder="E-mail"></input>
-        <input  type="password" id="senha" placeholder="Senha"></input>
-        <input type="text" id="wpp" placeholder="Whatsapp"></input>
-        <input  type="text" id="formacao" placeholder="Formação"></input>
-        <div>
-        <option value="" selected disabled id="opcao" >Tipo de usuario</option>
-        <select name="Tipo de usuario" required>
-        <option value="Aluno" id="aluno" >Aluno</option>
-        <option value="Professor" id="professor" >Professor</option>
-        </select>    
-        
+        <div class="box-form">
+          <form action="" method="POST" id="registerForm">
+            <h2>Cadastro de Usúario</h2>
+            <input type="text" id="nome" placeholder="Nome"></input>
+            <input type="text" id="email" placeholder="E-mail"></input>
+            <input type="password" id="senha" placeholder="Senha"></input>
+            <input type="text" id="wpp" placeholder="Whatsapp"></input>
+            <input type="text" id="formacao" placeholder="Formação"></input>
+            <div>
+              <option value="" selected disabled id="opcao" >Tipo de usuario</option>
+              <select name="Tipo de usuario" required>
+                <option value="Aluno" id="aluno" >Aluno</option>
+                <option value="Professor" id="professor" >Professor</option>
+              </select>
+            </div>
+            <input type="text" id="documentOf" />
+            <button type="submit">Registrar-se</button><br></br>
+
+          </form>
         </div>
-        <input type="text" id="documentOf"/>     
-        <button type="submit">Registrar-se</button><br></br>
-        
-        </form>
-        </div>
-        </div>
-        </div>
+      </div>
+    </div>
   );
 }
 
--- webtutor/src/pages/Login/index.js
+++ webtutor/src/pages/Login/index.js
@@ -2,36 +2,32 @@ import react from "react"
 
 import './index.css';
 import "./ComponentButton"
-import Logo from "./logo.PNG";
 import { Link } from "react-router-dom";
 
 function Login() {
 
   return (
     <div>
-        <div class="box">
-            
-            <div class="box-form">
-            <img src={Logo} class="logo"></img>
-            <form method="GET" id="loginForm">
-            <input 
-            type="text"
-            id ="login"
-            placeholder="Login">
+      <div class="box">
+        <div class="box-form">
+          <img src="./logo.PNG" class="logo"></img>
+          <form method="GET" id="loginForm">
+            <input
+              type="text"
+              id="login"
+              placeholder="Login">
             </input>
-            <input 
-            type="password"
-            id ="login"
-            placeholder="Password">
+            <input
+              type="password"
+              id="login"
+              placeholder="Password">
             </input>
-
             <button>Login</button>
-            <button><Link to="/Cadastro" class="link">Registrar-se</Link ></button>
-      
-        </form>
+            <button><Link to="/Cadastro" class="link">Registrar-se</Link></button>
+          </form>
 
-            </div>
         </div>
+      </div>
     </div>
   );
 }
